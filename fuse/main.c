c2b08b002d52e6f97f7713967a567e3cf0d532d5
relan <relan@users.noreply.github.com>
2020-05-23 18:36:19 +0300

    Pass through whitelisted options to FUSE.
    Allow users to specify the "nonempty" mount option.

--- fuse/main.c
+++ fuse/main.c
@@ -512,6 +512,23 @@ static char* add_fuse_options(char* options, const char* spec, bool ro)
 	return options;
 }
 
+static char* add_whitelisted_fuse_options(char* fuse_options,
+		const char* options)
+{
+	const char* whitelist[] = {"nonempty", NULL};
+	int i;
+
+	for (i = 0; whitelist[i] != NULL; i++)
+		if (exfat_match_option(options, whitelist[i]))
+		{
+			fuse_options = add_option(fuse_options, whitelist[i], NULL);
+			if (fuse_options == NULL)
+				return NULL;
+		}
+
+	return fuse_options;
+}
+
 static int fuse_exfat_main(char* mount_options, char* mount_point)
 {
 	char* argv[] = {"exfat", "-s", "-o", mount_options, mount_point, NULL};
@@ -564,6 +581,12 @@ int main(int argc, char* argv[])
 			if (exfat_options == NULL)
 			{
 				free(fuse_options);
+				return 1;
+			}
+			fuse_options = add_whitelisted_fuse_options(fuse_options, optarg);
+			if (fuse_options == NULL)
+			{
+				free(exfat_options);
 				return 1;
 			}
 			break;
